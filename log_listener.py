c0cf7d491f81550a92e6cf9c64c78186e99b2ba0
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2024-10-07 17:06:46 +1100

    updated function to process money

--- log_listener.py
+++ log_listener.py
@@ -2,17 +2,19 @@ import os
 from config import LOG_PATTERNS
 import sys
 import re
+import datetime
 
 class LogHandler:
 
     def __init__(self) -> None:
         self.log_patterns = LOG_PATTERNS
+        self.now = timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def processLogLine(self, log_line: str):
         print(f"Processing log line: {log_line}")
         self.getMethod(log_line)
         return None
-    
+
     def regexMatch(self, regex_expression: str, log_line: str):
         matches = re.findall(regex_expression, log_line)
         return matches
@@ -28,14 +30,20 @@ class LogHandler:
                     print(f"Method {func_name} not found or is not callable")
                 return
         print("Pattern doesn't match any method")
-    
+
     def processExpSP(self, log_line: str):
-        regex = r"(\d+)(?=拣起金钱)|(\d+)(?=\s*$)"
+        regex = r"(\d+)(?=经验)|(\d+)(?=灵气)|(\d+)(?=\s*$)"
         matches = self.regexMatch(regex, log_line)
         print("Processing EXP/SP gain...")
         print(f"Matches: {matches}")
 
-        return None
+    def processPickupMoney(self, log_line: str):
+        regex = r"(\d+)(?=拣起金钱)|(\d+)(?=\s*$)"
+        matches = self.regexMatch(regex, log_line)
+        roleid = matches[0][0]
+        money = matches[1][1]
+        print(f"Role ID: {roleid} picked up {money} money at {timestamp}")
+        return self.now, roleid, money
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
