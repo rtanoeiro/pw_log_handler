58bb65da22da63a053992b51438dc7c6fecf3295
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2024-10-07 14:17:24 +1100

    update listener to only read set number of lines

--- log_listerner.py
+++ log_listerner.py
@@ -1,19 +1,20 @@
-# Messages are in base64 format
-
-from collections import deque
 import os
 
 LOG_FILE = "world2.log"
 LAST_READ_LINE = os.environ["LAST_READ_LINE"]
-LINES_PER_STREAM = 10
+MAX_LINES = 1000
 
 def read_log_lines(file_to_read) -> list:
     with open(file_to_read, "r", encoding='gb2312') as file:
-        lines = file.readlines()
+        lines = file.readlines()[int(LAST_READ_LINE) : MAX_LINES]
         return lines
 
 
 last_lines = read_log_lines(file_to_read=LOG_FILE)
+
+with open("last_line.env", "w") as file:
+    last_line_string = f"LAST_READ_LINE={str(MAX_LINES + int(LAST_READ_LINE))}"
+    file.write(str(last_line_string))
 
 for line in last_lines:
     print(line)
