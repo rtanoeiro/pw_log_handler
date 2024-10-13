d55ebf014b7be53e3d6b79851b66b0804acaed5b
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2024-10-13 13:04:45 +1100

    update stop shell script

--- stop_log.sh
+++ stop_log.sh
@@ -3,9 +3,9 @@
 
 pwlogger_pid=$(pidof -x log_listener.sh)
 
-if [ -n "$pwlogify_pid" ]; then
+if [ -n "$pwlogger_pid" ]; then
     echo "Stopping pwlogger process (PID: $pwlogger_pid)"
-    kill $pwlogify_pid
+    kill $pwlogger_pid
 else
     echo "No pwlogger process found."
 fi
