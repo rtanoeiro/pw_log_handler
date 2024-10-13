f8bd8e8493aef0cd173766664e3a11e3725982e0
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2024-10-14 10:47:07 +1100

    removed blank lines

--- log_listener.sh
+++ log_listener.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
 log_script="log_listener.py"
 log_file_chat="home/logs/world2.chat"
 log_file_format="home/logs/world2.formatlog"
 log_file="home/logs/world2.log"
 read_from_start="false"
 files="$log_file_chat $log_file_format $log_file"
-
 process_log_line() {
     local log_line="$1"
     python3 "$log_script" "$log_line" 2>> logs/pw_log_handler.log
 }
-
 if [ "$read_from_start" = "true" ]; then
     if [ -f "$files" ]; then
         while read -r log_line; do
